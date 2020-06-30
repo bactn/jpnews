79708b5b1d6ad19ec23dbe7bdd6d3c3938cd9189
bactn <daihocbachkhoa92@gmail.com>
2020-07-01 00:00:54 +0900

    chang db server name

--- index.php
+++ index.php
@@ -128,17 +128,31 @@
                $tieude = $_POST['ftieude'];
                $noidung = $_POST['fnoidung'];
                $userID = 1;
-               $servername = "127.0.0.1:50659";
-               $username = "azure";
-               $password = "6#vWHD_$ )";
-               $dbname = "localdb";
                $date_now = date("Y-m-d h:i:s");
-
+               $servername = "";
+               $username = "";
+               $password = "";
+               $dbname = "";
+               
+               // Parsing connnection string
+               foreach ($_SERVER as $key => $value) {
+                   if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+                       continue;
+                   }
+                   
+                   $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+                   $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+                   $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+                   $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+               }
+               
                // Create connection
                $conn = new mysqli($servername, $username, $password, $dbname);
                // Check connection
                if ($conn->connect_error) {
-                  die("Connection failed: " . $conn->connect_error);
+                   die("Connection failed: " . $conn->connect_error);
+               } else {
+                   echo "connection successful<br/>";
                }
 
                $sql = "INSERT INTO bantin_tbl (menuID, tieude, noidung, userID, insertDate, urlImage)
