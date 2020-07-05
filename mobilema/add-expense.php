dd7fb5bc95a32a6d25edf2cb4f3ca9dbbdac01b3
bactn <daihocbachkhoa92@gmail.com>
2020-07-05 21:05:36 +0900

    add-expense

--- mobilema/add-expense.php
+++ mobilema/add-expense.php
@@ -6,126 +6,206 @@ if (strlen($_SESSION['mobilemauid'] == 0)) {
     header('location:logout.php');
 } else {
     if (isset($_POST['submit'])) {
-        $userid = $_SESSION['mobilemauid'];
-        $dateexpense = $_POST['dateexpense'];
-        $selectedCatID = $_POST['categoryOption'];
-        $date_now = date("Y-m-d");
-        $item = $_POST['item'];
-        $costitem = $_POST['costitem'];
-        $desURL = 'Testtesttest';
+        $accesskey = "X78SYPbFTckbY7jcu/Kxr4dkXGzTxLTlbwJuNY0a1ANypFpK9kNZySpGPqzXDBDnxle1vfWBY3y7X2Ivoq4jWQ==";
+        $storageAccount = 'faitostorage';
+        $filetoUpload = $_FILES['fileToUpload']['tmp_name'];
+        $containerName = 'public-contents';
+        $blobName = basename($_FILES["fileToUpload"]["name"]);
 
-        echo $queryStr;
-        $query = mysqli_query($con, "insert into bantin_tbl(menuID,tieude,noidung,userID,insertDate,urlImage) value('$selectedCatID','$item','$costitem','$userid','$date_now','$desURL')");
-        if ($query) {
-            echo "<script>alert('Expense has been added');</script>";
-            echo "<script>window.location.href='manage-expense.php'</script>";
-        } else {
-            echo $query;
-            echo "<script>alert('Something went wrong. Please try again');</script>";
+        $destinationURL = "";
+        if ($blobName != "") {
+           $destinationURL = "https://$storageAccount.blob.core.windows.net/$containerName/$blobName";
+           // Upload URL   
+           uploadBlob($filetoUpload, $storageAccount, $containerName, $blobName, $destinationURL, $accesskey);
         }
+
+        // Update DB
+        updateDB($destinationURL, $con);
+        $con->close();
     }
 }
+
+
+
+function uploadBlob($filetoUpload, $storageAccount, $containerName, $blobName, $destinationURL, $accesskey)
+{
+    $currentDate = gmdate("D, d M Y H:i:s T", time());
+    $handle = fopen($filetoUpload, 'r');
+    $fileLen = filesize($filetoUpload);
+    $headerResource = "x-ms-blob-cache-control:max-age=3600\nx-ms-blob-type:BlockBlob\nx-ms-date:$currentDate\nx-ms-version:2015-12-11";
+    $urlResource = "/$storageAccount/$containerName/$blobName";
+
+    $arraysign = array();
+    $arraysign[] = 'PUT';               /*HTTP Verb*/
+    $arraysign[] = '';                  /*Content-Encoding*/
+    $arraysign[] = '';                  /*Content-Language*/
+    $arraysign[] = $fileLen;            /*Content-Length (include value when zero)*/
+    $arraysign[] = '';                  /*Content-MD5*/
+    $arraysign[] = 'image/png';         /*Content-Type*/
+    $arraysign[] = '';                  /*Date*/
+    $arraysign[] = '';                  /*If-Modified-Since */
+    $arraysign[] = '';                  /*If-Match*/
+    $arraysign[] = '';                  /*If-None-Match*/
+    $arraysign[] = '';                  /*If-Unmodified-Since*/
+    $arraysign[] = '';                  /*Range*/
+    $arraysign[] = $headerResource;     /*CanonicalizedHeaders*/
+    $arraysign[] = $urlResource;        /*CanonicalizedResource*/
+
+    $str2sign = implode("\n", $arraysign);
+
+    $sig = base64_encode(hash_hmac('sha256', urldecode(utf8_encode($str2sign)), base64_decode($accesskey), true));
+    $authHeader = "SharedKey $storageAccount:$sig";
+
+    $headers = [
+        'Authorization: ' . $authHeader,
+        'x-ms-blob-cache-control: max-age=3600',
+        'x-ms-blob-type: BlockBlob',
+        'x-ms-date: ' . $currentDate,
+        'x-ms-version: 2015-12-11',
+        'Content-Type: image/png',
+        'Content-Length: ' . $fileLen
+    ];
+
+    $ch = curl_init($destinationURL);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_INFILE, $handle);
+    curl_setopt($ch, CURLOPT_INFILESIZE, $fileLen);
+    curl_setopt($ch, CURLOPT_UPLOAD, true);
+    $result = curl_exec($ch);
+
+    echo ('Result<br/>');
+    print_r($result);
+
+    // echo ('Error<br/>');
+    print_r(curl_error($ch));
+    // upload image
+    curl_close($ch);
+}
+
+function updateDB($destinationURL, $con)
+{
+    $userid = $_SESSION['mobilemauid'];
+    $selectedCatID = $_POST['categoryOption'];
+    $date_now = date("Y-m-d");
+    $item = $_POST['item'];
+    $costitem = $_POST['costitem'];
+
+    $query = mysqli_query($con, "insert into bantin_tbl(menuID,tieude,noidung,userID,insertDate,urlImage) value('$selectedCatID','$item','$costitem','$userid','$date_now','$destinationURL')");
+    if ($query) {
+        echo "<script>alert('Expense has been added');</script>";
+        echo "<script>window.location.href='manage-expense.php'</script>";
+    } else {
+        echo $query;
+        echo "<script>alert('Something went wrong. Please try again');</script>";
+    }
+
+    
+}
 ?>
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FAITO - 掲示板詳細登録</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/font-awesome.min.css" rel="stylesheet">
-        <link href="css/datepicker3.css" rel="stylesheet">
-        <link href="css/styles.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FAITO - 掲示板詳細登録</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/datepicker3.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 
-        <!--Custom Font-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-        <!--[if lt IE 9]>
+    <!--Custom Font-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
-    </head>
+</head>
 
-    <body>
-        <?php include_once('includes/header.php'); ?>
-        <?php include_once('includes/sidebar.php'); ?>
+<body>
+    <?php include_once('includes/header.php'); ?>
+    <?php include_once('includes/sidebar.php'); ?>
 
-        <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-            <div class="row">
-                <ol class="breadcrumb">
-                    <li><a href="#"> <em class="fa fa-home"></em>
-                        </a></li>
-                    <li class="active">掲示板登録</li>
-                </ol>
-            </div>
-            <!--/.row-->
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="#"> <em class="fa fa-home"></em>
+                    </a></li>
+                <li class="active">掲示板登録</li>
+            </ol>
+        </div>
+        <!--/.row-->
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">掲示板詳細登録</div>
-                        <div class="panel-body">
-                            <p style="font-size: 16px; color: red" align="center"> <?php if ($msg) {
-                                                                                        echo $msg;
-                                                                                    } ?> </p>
-                            <div class="col-md-12">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">掲示板詳細登録</div>
+                    <div class="panel-body">
+                        <p style="font-size: 16px; color: red" align="center"> <?php if ($msg) {
+                                                                                    echo $msg;
+                                                                                } ?> </p>
+                        <div class="col-md-12">
 
-                                <form role="form" method="post" action="">
-                                    <!-- メニュー-->
-                                    <div class="form-group">
-                                        <label>メニュー</label>
-                                        <select class="form-control" type="option" value="" name="categoryOption" required="true">
-                                            <option value="1">ニュース</option>
-                                            <option value="2">生活</option>
-                                            <option value="3">学習</option>
-                                            <option value="3">お仕事</option>
-                                        </select>
-                                    </div>
-                                    <!-- タイトル -->
-                                    <div class="form-group">
-                                        <label>タイトル</label>
-                                        <input type="text" class="form-control" name="item" value="" required="true" placeholder="タイトルを入力して下さい。">
-                                    </div>
-                                    <!-- 内容 -->
-                                    <div class="form-group">
-                                        <label>内容</label>
-                                        <!-- input class="form-control" type="text" value="" required="true" name="costitem" -->
-                                        <textarea class="form-control" value="" required="true" name="costitem" placeholder="内容を入力して下さい。"></textarea>
-                                    </div>
-                                    <!-- ファイアプローチ-->
-                                    <div class="form-group">
-                                        <label>画像アップロード</label>
-                                        <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" required="true">
-                                    </div>
-                                    <!-- 登録 -->
-                                    <div class="form-group has-success">
-                                        <button type="submit" class="btn btn-primary" name="submit">登録</button>
-                                    </div>
+                            <form role="form" method="post" action="">
+                                <!-- メニュー-->
+                                <div class="form-group">
+                                    <label>メニュー</label>
+                                    <select class="form-control" type="option" value="" name="categoryOption" required="true">
+                                        <option value="1">ニュース</option>
+                                        <option value="2">生活</option>
+                                        <option value="3">学習</option>
+                                        <option value="3">お仕事</option>
+                                    </select>
+                                </div>
+                                <!-- タイトル -->
+                                <div class="form-group">
+                                    <label>タイトル</label>
+                                    <input type="text" class="form-control" name="item" value="" required="true" placeholder="タイトルを入力して下さい。">
+                                </div>
+                                <!-- 内容 -->
+                                <div class="form-group">
+                                    <label>内容</label>
+                                    <!-- input class="form-control" type="text" value="" required="true" name="costitem" -->
+                                    <textarea class="form-control" value="" required="true" name="costitem" placeholder="内容を入力して下さい。"></textarea>
+                                </div>
+                                <!-- ファイアプローチ-->
+                                <div class="form-group">
+                                    <label>画像アップロード</label>
+                                    <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" required="true">
+                                </div>
+                                <!-- 登録 -->
+                                <div class="form-group has-success">
+                                    <button type="submit" class="btn btn-primary" name="submit">登録</button>
+                                </div>
 
-                            </div>
-
-                            </form>
                         </div>
+
+                        </form>
                     </div>
                 </div>
-                <!-- /.panel-->
             </div>
-            <!-- /.col-->
-            <?php include_once('includes/footer.php'); ?>
+            <!-- /.panel-->
         </div>
-        <!-- /.row -->
-        </div>
-        <!--/.main-->
+        <!-- /.col-->
+        <?php include_once('includes/footer.php'); ?>
+    </div>
+    <!-- /.row -->
+    </div>
+    <!--/.main-->
 
-        <script src="js/jquery-1.11.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/chart.min.js"></script>
-        <script src="js/chart-data.js"></script>
-        <script src="js/easypiechart.js"></script>
-        <script src="js/easypiechart-data.js"></script>
-        <script src="js/bootstrap-datepicker.js"></script>
-        <script src="js/custom.js"></script>
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/chart.min.js"></script>
+    <script src="js/chart-data.js"></script>
+    <script src="js/easypiechart.js"></script>
+    <script src="js/easypiechart-data.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/custom.js"></script>
 
-    </body>
+</body>
 
-    </html>
+</html>
