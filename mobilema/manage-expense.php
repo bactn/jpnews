d9ecbb84078c401c8cbfadde04d7db27831fbdff
bactn <daihocbachkhoa92@gmail.com>
2020-07-07 00:31:13 +0900

    add menu

--- mobilema/manage-expense.php
+++ mobilema/manage-expense.php
@@ -84,8 +84,8 @@ if (strlen($_SESSION['mobilemauid'] == 0)) {
 												<tr>
 													<td><?php echo $cnt; ?></td>
 													<td><?php echo $row['menu']; ?></td>
-													<td style="text-overflow: clip;"><?php $row['tieude']; ?></td>
-													<td style="text-overflow: clip;"> <?php echo $row['noidung']; ?></td>
+													<td style="text-overflow: ellipsis;"><?php $row['tieude']; ?></td>
+													<td style="text-overflow: ellipsis;"> <?php echo $row['noidung']; ?></td>
 													<td><?php echo $row['insertDate']; ?></td>
 													<td>
 														<a href="add-expense.php?editid=<?php echo $row['bantinID']; ?>">編集</a> -
