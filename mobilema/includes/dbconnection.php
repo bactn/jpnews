4161197ad528cd8d939e9d697c0f0de1305c361a
bactn <daihocbachkhoa92@gmail.com>
2020-07-31 10:26:38 +0900

    change charset db connect

--- mobilema/includes/dbconnection.php
+++ mobilema/includes/dbconnection.php
@@ -17,6 +17,8 @@
 
  // Create connection
  $con = new mysqli($servername, $username, $password, $dbname);
+ $con->set_charset("utf8");
+
  // Check connection
  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
